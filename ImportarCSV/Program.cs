f9e5e165d3afdc079a9c154ac6d818c6d02f2d78
Gonzalo Andres Diaz <gonchi.diaz125@gmail.com>
2024-05-16 14:58:42 -0300

    Agrego try catch al leer cada linea del archivo

--- ImportarCSV/Program.cs
+++ ImportarCSV/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ImportarCSV;
+using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.IO;
@@ -6,26 +7,43 @@ using System.IO;
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hola! soy el importador de CSV");
 
+string currentDirectory = Environment.CurrentDirectory;
+Console.WriteLine("Current Directory: " + currentDirectory);
+
+
 List<PersonaImportada> personasImportadas = new List<PersonaImportada>();
 
-using (var reader = new StreamReader(@"C:\desarrollo\EjemploArchivoCsv\ImportarCSV\personas1.csv"))
+using (var reader = new StreamReader(@"..\..\..\..\personas2.csv"))
 {
-    while (!reader.EndOfStream)
-    {
-        var linea = reader.ReadLine();
-        var valores = linea.Split(',');
-
-        var unaPersona = new PersonaImportada()
+	while (!reader.EndOfStream)
+	{
+        string linea = string.Empty;
+        try
         {
-            Documento = valores[0],
-            Nombre = valores[1],
-            SegundoNombre = valores[2],
-            Apellido = valores[3],
-        };
+			linea = reader.ReadLine();
+			var valores = linea.Split(',');
 
-        personasImportadas.Add(unaPersona);
-    }
+			var unaPersona = new PersonaImportada()
+			{
+				Documento = valores[0],
+				Nombre = valores[1],
+				SegundoNombre = valores[2],
+				Apellido = valores[3],
+			};
+
+			personasImportadas.Add(unaPersona);
+		}
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error al procesar la linea: {linea}");
+            Console.WriteLine(ex.Message);
+            
+        }		
+	}
 }
+
+return;
+
 
 List<Persona> personas = new List<Persona>();
 
@@ -96,67 +114,8 @@ public class Persona
     public string Apellido { get; set; }
 }
 
-public class PersonaRepository
-{
-    string connectionString = "Server=localhost;Database=ImportarCsvBD;Trusted_Connection=True;MultipleActiveResultSets=true";
 
-    public int Insert(Persona persona)
-    {
-        int idInsertado = 0;
-        using (SqlConnection connection = new SqlConnection(connectionString))
-        {
-            string query = "INSERT INTO Personas (Documento, Nombre, SegundoNombre, Apellido) VALUES (@Documento, @Nombre, @SegundoNombre, @Apellido); SELECT SCOPE_IDENTITY();";
-            SqlCommand command = new SqlCommand(query, connection);
 
-            command.Parameters.AddWithValue("@Documento", persona.Documento);
-            command.Parameters.AddWithValue("@Nombre", persona.Nombre);
-            command.Parameters.AddWithValue("@SegundoNombre", persona.SegundoNombre); // Aquí se corrigió el parámetro
-            command.Parameters.AddWithValue("@Apellido", persona.Apellido);
 
-            connection.Open();
-            idInsertado = Convert.ToInt32(command.ExecuteScalar());
-        }
-
-        return idInsertado; // Aquí se devuelve el ID insertado
-    }
-    public bool ExistePersonaPorDocumento(int documento)
-    {
-        bool existe = false;
-        using (SqlConnection connection = new SqlConnection(connectionString))
-        {
-            string query = "SELECT COUNT(1) FROM Personas WHERE Documento = @Documento";
-            SqlCommand command = new SqlCommand(query, connection);
-            command.Parameters.AddWithValue("@Documento", documento);
-
-            connection.Open();
-            existe = Convert.ToBoolean(command.ExecuteScalar());
-        }
-
-        return existe;
-    }
-}
-
-public class PersonaLogic
-{
-    private readonly PersonaRepository personaRepository;
-
-    public PersonaLogic()
-    {
-        this.personaRepository = new PersonaRepository();
-    }
-
-    public int InsertarPersona(Persona persona)
-    {
-        if (!personaRepository.ExistePersonaPorDocumento(persona.Documento))
-        {
-            return personaRepository.Insert(persona);
-        }
-        else
-        {
-            Console.WriteLine($"La persona con documento {persona.Documento} ya existe.");
-            return 0; 
-        }
-    }
-}
 
 
