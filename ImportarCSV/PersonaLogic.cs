73934b2f27f2b5940db44edbf62f6d5cc0089235
Gonzalo Andres Diaz <gonchi.diaz125@gmail.com>
2024-05-16 14:55:04 -0300

    Agrego try catch englobando toda la lectura del archivo

---
+++ ImportarCSV/PersonaLogic.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImportarCSV
+{
+	public class PersonaLogic
+	{
+		private readonly PersonaRepository personaRepository;
+
+		public PersonaLogic()
+		{
+			this.personaRepository = new PersonaRepository();
+		}
+
+		public int InsertarPersona(Persona persona)
+		{
+			if (!personaRepository.ExistePersonaPorDocumento(persona.Documento))
+			{
+				return personaRepository.Insert(persona);
+			}
+			else
+			{
+				Console.WriteLine($"La persona con documento {persona.Documento} ya existe.");
+				return 0;
+			}
+		}
+	}
+}
---
+++ ImportarCSV/PersonaRepository.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImportarCSV
+{
+	public class PersonaRepository
+	{
+		string connectionString = "Server=localhost;Database=ImportarCsvBD;Trusted_Connection=True;MultipleActiveResultSets=true";
+
+		public int Insert(Persona persona)
+		{
+			int idInsertado = 0;
+			using (SqlConnection connection = new SqlConnection(connectionString))
+			{
+				string query = "INSERT INTO Personas (Documento, Nombre, SegundoNombre, Apellido) VALUES (@Documento, @Nombre, @SegundoNombre, @Apellido); SELECT SCOPE_IDENTITY();";
+				SqlCommand command = new SqlCommand(query, connection);
+
+				command.Parameters.AddWithValue("@Documento", persona.Documento);
+				command.Parameters.AddWithValue("@Nombre", persona.Nombre);
+				command.Parameters.AddWithValue("@SegundoNombre", persona.SegundoNombre); // Aquí se corrigió el parámetro
+				command.Parameters.AddWithValue("@Apellido", persona.Apellido);
+
+				connection.Open();
+				idInsertado = Convert.ToInt32(command.ExecuteScalar());
+			}
+
+			return idInsertado; // Aquí se devuelve el ID insertado
+		}
+		public bool ExistePersonaPorDocumento(int documento)
+		{
+			bool existe = false;
+			using (SqlConnection connection = new SqlConnection(connectionString))
+			{
+				string query = "SELECT COUNT(1) FROM Personas WHERE Documento = @Documento";
+				SqlCommand command = new SqlCommand(query, connection);
+				command.Parameters.AddWithValue("@Documento", documento);
+
+				connection.Open();
+				existe = Convert.ToBoolean(command.ExecuteScalar());
+			}
+
+			return existe;
+		}
+	}
+}
